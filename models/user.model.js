ab1100f6111e312be0356f4effc21173bf664dac
= <=>
2025-01-04 04:09:18 -0500

    build: user model

---
+++ models/user.model.js
@@ -0,0 +1,17 @@
+const User = {
+  username: "",
+  password: "",
+  profile: {
+    vision: "",
+    mission: "",
+    projects: [
+      {
+        title: "",
+        description: "",
+        media: [],
+      },
+    ],
+  },
+};
+
+export default User;
