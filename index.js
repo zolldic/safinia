b8c517cf95b6058b440e7ddd907b041061d2b89e
= <=>
2025-01-06 02:13:54 -0500

    clean up entry points

--- index.js
+++ index.js
@@ -1,48 +1,13 @@
 import express from "express";
-
-import JSONClient from "./utilite/jsonClient.js";
+import handler from "./routes/handler.js";
 
 const app = express();
-
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.status(200).json({
-    message: "hi",
-  });
-});
+handler(app);
 
-app.get("/api/grants", (req, res) => {
-  // fetch all grants from database
-  const data = JSONClient.all("grants");
-  res.status(201).json(data);
-});
+const port = process.env.PORT || 3000;
 
-const ORG = {
-  name: "Salam",
-  projects: [
-    {
-      title: "",
-      duration: "",
-    },
-  ],
-};
-
-app.post("/api/profile", (req, res) => {
-  // set org profile
-});
-
-app.post("/api/write-proposal", (req, res) => {
-  // write proposal
-});
-
-/*
-app.post("/api/grants/new", (req, res) => {
-  // create new grant
-});
-*/
-
-const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
 });
